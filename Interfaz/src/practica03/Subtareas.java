765b49156f111de477b38ada4968fa2cf1f481f3
raulhg16 <raul.hergom.1@educa.jcyl.es>
2025-03-07 17:50:31 +0100

    clase subtarea

---
+++ Interfaz/src/practica03/Subtareas.java
@@ -0,0 +1,61 @@
+package practica03;
+
+public class Subtareas {
+
+    private int id;
+    private String descripcion;
+    private String prioridad;
+    private String fechaLimite;
+    private boolean completada;
+
+    public Subtareas(int id, String descripcion, String prioridad, String fechaLimite, boolean completada) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.fechaLimite = fechaLimite;
+        this.completada = completada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public String getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public void setFechaLimite(String fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Descripcion: " + descripcion + ", Prioridad: " + prioridad +
+                ", Fecha Limite: " + fechaLimite + ", Completada: " + (completada ? "Si" : "No");
+    }
+	
+}
